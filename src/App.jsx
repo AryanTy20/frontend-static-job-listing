7156009fe69285152da54eeae38b281c27547773
AryanTy20 <devty007@gmail.com>
2022-09-08 08:47:52 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useEffect, useState, useRef } from "react";
+import "./app.scss";
+
+const App = () => {
+  return (
+    <>
+      <div className="main">job</div>
+    </>
+  );
+};
+
+export default App;
